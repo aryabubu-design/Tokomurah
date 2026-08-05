d6dfeaaef9c26beaef69940e2146d2eee27bacb4
aryabubu-design <aryabubu0806@gmail.com>
2026-08-05 02:59:29 +0000

    langkah 3 tambahan severjs

---
+++ backend/sever.js
@@ -0,0 +1,21 @@
+
+
+// Mengimpor library express yang sudah diinstal
+const express = require("express");
+
+// Membuat instance aplikasi Express
+const app = express();
+const PORT = 3000;
+
+// Middleware bawaan agar Express bisa membaca JSON dari request
+app.use(express.json());
+
+// Route paling dasar, hanya untuk mengecek server hidup
+app.get("/", (req, res) => {
+    res.send("Selamat datang di API Tokomurah!");
+});
+
+// Menjalankan server dan mendengarkan di PORT yang ditentukan
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
